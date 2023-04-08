733bfe0e9d105e1be59c36befc374def7eea893f
Saveliy Kholin <holins12@gmail.com>
2023-04-08 16:30:44 -0700

    copy from replit

--- main.py
+++ main.py
@@ -1,68 +1,54 @@
 import json
 
-import quart
-import quart_cors
-from quart import request
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, JSONResponse
 
-# Note: Setting CORS to allow chat.openapi.com is required for ChatGPT to access your plugin
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+app = FastAPI()
 
 _TODOS = {}
 
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+@app.post("/todos/{username}")
+async def add_todo(username: str, todo: str):
+  if username not in _TODOS:
+    _TODOS[username] = []
+  _TODOS[username].append(todo)
+  return JSONResponse(content='OK', status_code=200)
 
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+@app.get("/todos/{username}")
+async def get_todos(username: str):
+  return JSONResponse(content=_TODOS.get(username, []), status_code=200)
 
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.delete("/todos/{username}")
+async def delete_todo(username: str, todo_idx: int):
+  if username inx _TODOS and 0 <= todo_idx < len(_TODOS[username]):
+    _TODOS[username].pop(todo_idx)
+  return JSONResponse(content='OK', status_code=200)
 
 
 @app.get("/logo.png")
 async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
+  return FileResponse('logo.png')
 
-###
 
 @app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    host = request.headers['Host']
-    with open("ai-plugin.json") as f:
-        text = f.read()
-        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the manifest
-        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
-        return quart.Response(text, mimetype="text/json")
+async def plugin_manifest(request: Request):
+  host = request.headers['host']
+  with open("ai-plugin.json") as f:
+    text = f.read().replace("PLUGIN_HOSTNAME", f"https://{host}")
+  return JSONResponse(content=json.loads(text))
 
 
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    host = request.headers['Host']
-    with open("openapi.yaml") as f:
-        text = f.read()
-        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the OpenAPI spec
-        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
-        return quart.Response(text, mimetype="text/yaml")
-
-
-def main():
-    app.run(debug=True, host="0.0.0.0", port=5002)
+@app.get("/openapi.json")
+async def openapi_spec(request: Request):
+  host = request.headers['host']
+  with open("openapi.json") as f:
+    text = f.read().replace("PLUGIN_HOSTNAME", f"https://{host}")
+  return JSONResponse(content=text, media_type="text/json")
 
 
 if __name__ == "__main__":
-    main()
+  import uvicorn
+  uvicorn.run(app, host="0.0.0.0", port=5002)
