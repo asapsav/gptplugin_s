13e627a66b8d8fe7f5520034d7938c8786115f65
Saveliy Kholin <holins12@gmail.com>
2023-04-08 22:40:58 -0700

    complete landing page redesign

--- scripts.js
+++ scripts.js
@@ -3,12 +3,17 @@ const characters = [
     document.getElementById('character2'),
     document.getElementById('character3'),
     document.getElementById('character4'),
-    document.getElementById('character5')
+    document.getElementById('character5'),
+    document.getElementById('character6'),
+    document.getElementById('character7'),
+    document.getElementById('character8'),
+    document.getElementById('character9'),
+    document.getElementById('character10')
   ];
   
   function animateCharacter(character) {
-    const x = Math.random() * (window.innerWidth - character.clientWidth);
-    const y = Math.random() * (window.innerHeight - character.clientHeight);
+    const x = Math.random() * (window.innerWidth - character.clientWidth * 2) + character.clientWidth / 2;
+    const y = Math.random() * (window.innerHeight - character.clientHeight * 2) + character.clientHeight / 2;
     const duration = Math.random() * 3000 + 3000;
   
     character.animate([
@@ -24,5 +29,19 @@ const characters = [
     }, duration);
   }
   
-  characters.forEach(character => animateCharacter(character));
+  characters.forEach(character => {
+    animateCharacter(character);
+  
+    // Add an event listener for the 'click' event to apply the spinning animation
+    character.addEventListener('click', () => {
+      character.animate([
+        { transform: 'rotate(0deg)' },
+        { transform: 'rotate(360deg)' },
+      ], {
+        duration: 500,
+        iterations: 1,
+        easing: 'linear',
+      });
+    });
+  });
   
